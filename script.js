d1d4367e8b335b518f39a1374a744d72528e828a
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2022-12-29 22:57:10 +0000

    exercice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+ //avec une boucle for
 let x = 6
 let y=""
 for(let i=0;i<x;i++){
@@ -7,4 +7,17 @@ for(let i=0;i<x;i++){
 }
 
 
+//avec deux boucle for 
+let n= 6
+for(let i=0;i<n;i++){
+    let point = i+1;
+    let z=""
+    for (let j = 0; j < point; j++) {
 
+       z =z +" * "
+        
+    }
+    console.log(z)
+
+    
+}
